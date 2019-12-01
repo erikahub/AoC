09c7511f521cc28295a22d17536d09850cfd7418
Erika <huettebraeucker.robin@gmail.com>
2019-12-01 23:13:47 +0100

    WAIYUNOWORK

---
+++ Day1/First.py
@@ -0,0 +1,17 @@
+allModules = list()
+
+# tests = [ 12, 14, 1969, 100756]
+# results = [2, 2, 654, 33583]
+
+with open("Day1/DataFirst.txt", 'r') as file:
+    for line in file:
+        allModules.append(int(line[:-1]))
+    
+# print(allModules)
+# print(len(allModules))
+
+result = 0
+for val in allModules:
+    result+=((val//3)-2)
+
+print(result)
