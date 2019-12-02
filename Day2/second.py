88fed8773486a1572d6a48bb369d418b649cfe35
Erika <huettebraeucker.robin@gmail.com>
2019-12-02 17:09:03 +0100

    Update second.py
    Found solution and added problem description

--- Day2/second.py
+++ Day2/second.py
@@ -1,46 +1,66 @@
 """
-list of intcodes are used as initial state of memory[...]
-
-For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters
-
+FROM: https://adventofcode.com/2019/day/2
+--- Part Two ---
+"Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. 
+Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+"However, your current priority should be to complete your gravity assist around the Moon. 
+For this mission to succeed, we should settle on some terminology for the parts you've already built."
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. 
+When you run an Intcode program, make sure to start by initializing memory to the program's values. 
+A position in memory is called an address (for example, the first value in memory is at "address 0").
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. 
+The values used immediately after an opcode, if any, are called the instruction's parameters. 
+For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. 
+The instruction 99 contains only an opcode and has no parameters.
 The address of the current instruction is called the instruction pointer; it starts at 0. 
-After an instruction finishes, the instruction pointer increases by the number 
-of values in the instruction;
-
-!!! you need to determine what pair of inputs produces the output 19690720
-
+After an instruction finishes, the instruction pointer increases by the number of values in the instruction; 
+until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. 
+(The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+"With terminology out of the way, we're ready to proceed. 
+To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. 
+In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. 
+Each of the two input values will be between 0 and 99, inclusive.
+Once the program has halted, its output is available at address 0, also just like before. 
+Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) 
+- in other words, don't reuse memory from a previous attempt.
+Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? 
+(For example, if noun=12 and verb=2, the answer would be 1202.)
 """
 
-def findPair(split: list):
+def findPair(opList: list):
     for x in range(100):
         for y in range(100):
-            split[1]=x
-            split[2]=y
+            listCopy = opList.copy()
+            listCopy[1]=x
+            listCopy[2]=y
             i=0
-            while split[i] != 99:
-                opcode = split[i]
-                #i < len(split) and 
+            length = len(listCopy)
+            while listCopy[i] != 99:
+                if (length < listCopy[i] or length < listCopy[i+1] or length < listCopy[i+2] or length < listCopy[i+3]):
+                    print('invalid index')
+                    i+=4
+                    break
+                opcode = listCopy[i]
                 if opcode == 1:
-                    print('pos(%i) = pos(%i) + pos(%i) with i = %i' % (split[i+3], split[i+1], split[i+2], i))
-                    print('%i = %i + %i' % ( split[split[i+1]]+split[split[i+2]],  split[split[i+1]], split[split[i+2]]))
-                    split[split[i+3]] = split[split[i+1]] + split[split[i+2]]
+                    # print('pos(%i) equalling %i = pos(%i) + pos(%i) with i = %i' % (listCopy[i+3], listCopy[listCopy[i+3]], listCopy[i+1], listCopy[i+2], i))
+                    # print('%i = %i + %i' % ( listCopy[listCopy[i+1]]+listCopy[listCopy[i+2]],  listCopy[listCopy[i+1]], listCopy[listCopy[i+2]]))
+                    listCopy[listCopy[i+3]] = listCopy[listCopy[i+1]] + listCopy[listCopy[i+2]]
                 elif opcode == 2:
-                    split[split[i+3]] = split[split[i+1]] * split[split[i+2]]
+                    # print('pos(%i) equalling %i = pos(%i) + pos(%i) with i = %i' % (listCopy[i+3], listCopy[listCopy[i+3]], listCopy[i+1], listCopy[i+2], i))
+                    # print('%i = %i + %i' % ( listCopy[listCopy[i+1]]*listCopy[listCopy[i+2]],  listCopy[listCopy[i+1]], listCopy[listCopy[i+2]]))
+                    listCopy[listCopy[i+3]] = listCopy[listCopy[i+1]] * listCopy[listCopy[i+2]]
                 else:
                     print('unknown operation')
                 i+=4
-            if split[0] == 19690720:
-                print(x, y)
-    # return list()
-        	# print(split[0])
+            if listCopy[0] == 19690720:
+                print(x, y, x*100 + y)
+
 
 
 split = list()
-# split = [ int(i) for i in line.split(',') ]
-
 #read from file
 with open('Day2/input.txt') as file:
     split = [ int(x) for x in file.readlines(1)[0].split(',') ]
     
-print(split[56:58])
-# print(findPair(split))
+findPair(split)
