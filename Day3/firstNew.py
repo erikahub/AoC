d24b19da227e0fd850a87e4093eb26304b3023e6
Erika <huettebraeucker.robin@gmail.com>
2019-12-03 22:54:00 +0100

    Update firstNew.py

--- Day3/firstNew.py
+++ Day3/firstNew.py
@@ -9,3 +9,32 @@ wireB = list()
 wireA, wireB = open('Day3/wirepaths.txt').read().split('\n')
 wireA, wireB = [x.split(',') for x in [wireA, wireB]]
 
+pathA = [*((a[0], a[1:]) for a in wireA)]
+pathB = [*((b[0], b[1:]) for b in wireB)]
+#horizontal
+h = ['R', 'L']
+#vertical
+v = ['U', 'D']
+currentA = origin
+currentB = origin
+for a in pathA:
+    dirA = a[0]
+    stepA = a[1]
+    for b in pathB:
+        dirB = b[0]
+        stepB = b[1]
+        # assert dirA, dirB in ['R', 'L', 'D', 'U']
+        if dirA in h:
+            if dirB in h:
+                if currentA[1] == currentB[1]:
+                    if abs(stepA)-abs(currentA[0]) <= abs(stepB) - abs(currentB):
+                        
+
+    if dirA == 'R':
+        currentA[0]+=stepA
+    elif dirA == 'L':
+        currentA[0]+=stepA
+    elif dirA == 'D':
+        currentA[1]+=stepA
+    elif dirA == 'U':
+        currentA[1]+=stepA
